dca2cf6a727893390a0c3fd1e4f9ccbb7d2cf6bb
Bill Tsapalos <littleprog@gmail.com>
2019-12-07 00:39:12 +0200

    Improve min -n- max values for the actual car

--- src/ServoControl.kt
+++ src/ServoControl.kt
@@ -18,45 +18,45 @@ fun main(args: Array<String>){
     // Set raw value for this servo driver - 50 to 250
 
     // Example 1
-    println("Go to 125") //middle
-    servo0.servoPulseWidth = 125
+    println("Go to 150") //middle
+    servo0.servoPulseWidth = 150
     println("Went to ${servo0.servoPulseWidth}")
     Thread.sleep(1550)
-    println("Go to 50") //min value
-    servo0.servoPulseWidth = 50
+    println("Go to 65") //min value 50
+    servo0.servoPulseWidth = 65
     println("Went to ${servo0.servoPulseWidth}")
     Thread.sleep(1550)
-    println("Go to 250") //max value
-    servo0.servoPulseWidth = 250
+    println("Go to 235") //max value 250
+    servo0.servoPulseWidth = 235
     println("Went to ${servo0.servoPulseWidth}")
     Thread.sleep(1550)
     println("Go to 150")
     servo0.servoPulseWidth = 150
     println("Went to ${servo0.servoPulseWidth}")
     Thread.sleep(1550)
-    println("Go to 0") // wrong value
-    servo0.servoPulseWidth = 0
-    println("Went to ${servo0.servoPulseWidth}")
-    Thread.sleep(1550)
+    //println("Go to 0") // wrong value
+    //servo0.servoPulseWidth = 0
+    //println("Went to ${servo0.servoPulseWidth}")
+    //Thread.sleep(1550)
 
     // Example 2
     val out = PrintWriter(FileOutputStream("/dev/servoblaster"), true)
-    println("Go to 50 again")
-    out.println("5=50")
+    println("Go to 65 again")
+    out.println("5=65")
     out.flush()
     Thread.sleep(3000)
-    println("Go to 250 again")
-    out.println("5=250")
+    println("Go to 235 again")
+    out.println("5=235")
     out.flush()
     Thread.sleep(3000)
     println("Go to 150 again")
     out.println("5=150")
     out.flush()
     Thread.sleep(3000)
-    println("Go to 0 again")
-    out.println("5=0")
-    out.flush()
-    Thread.sleep(3000)
+    //println("Go to 0 again")
+    //out.println("5=0")
+    //out.flush()
+    //Thread.sleep(3000)
 
     // kill servo blaster
     //Runtime.getRuntime().exec("killall servod")
